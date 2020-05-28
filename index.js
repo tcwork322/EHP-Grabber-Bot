d94bfc9b9cd0d5157247cde18cb08d0cbd98688d
tcwork322 <tc.work322@gmail.com>
2020-05-28 06:24:36 +0100

    ++ Quote Function and better UX dialogue

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hiscores = require('osrs-json-hiscores');
 // Importing this allows you to access the environment variables of the running node process
 require('dotenv').config();
 
+
 const client = new Discord.Client();
 const puppeteer = require('puppeteer');
 const prefix = '$!';
@@ -13,17 +14,44 @@ let yearlyEhp;
 let allEhp;
 
 // Scrape Functions
-async function updateLabs(updateUrl) {
-	const browser = await puppeteer.launch();
+
+/* async function getQuote(quoteUrl) {
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disablesetuid-sandobx'],
+	});
 	const page = await browser.newPage();
-	page.goto(updateUrl).catch(e => console.log('Expected navigation error, no worries.'));
-	setTimeout(function() {
-		browser.close();
-	}, 10000);
+	await page.goto(quoteUrl).catch(e => console.log(e));
+	// Get Quote
+	const [el] = await page.$x('/html/body/div[8]/div/div/div/div/div/div/table[2]/tbody/tr[1]/td[2]/b');
+	const txt = await el.getProperty('textContent');
+	const rawTxt = await txt.jsonValue();
+	console.log(rawTxt);
+	quote = await rawTxt;
+	// Get Author
+	const [el2] = await page.$x('/html/body/div[8]/div/div/div/div/div/div/table[2]/tbody/tr[1]/td[2]/p/a');
+	const txt2 = await el2.getProperty('textContent');
+	const rawTxt2 = await txt2.jsonValue();
+	console.log(rawTxt2);
+	author = await rawTxt2;
+	browser.close();
+} */
+
+async function updateLabs(updateUrl) {
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
+	const page = await browser.newPage();
+	await page.goto(updateUrl).catch(e => console.log('Expected navigation error, no worries.'));
+	browser.close();
 }
 
 async function scrapeLabsAlltime(labsUrl) {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 	const page = await browser.newPage();
 	await page.goto(labsUrl);
 
@@ -36,7 +64,10 @@ async function scrapeLabsAlltime(labsUrl) {
 }
 
 async function scrapeLabsYearly(labsUrl) {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 	const page = await browser.newPage();
 	await page.goto(labsUrl);
 
@@ -49,7 +80,10 @@ async function scrapeLabsYearly(labsUrl) {
 }
 
 async function scrapeLabsMonthly(labsUrl) {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 	const page = await browser.newPage();
 	await page.goto(labsUrl);
 
@@ -61,7 +95,10 @@ async function scrapeLabsMonthly(labsUrl) {
 	browser.close();
 }
 async function scrapeLabsWeekly(labsUrl) {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 	const page = await browser.newPage();
 	await page.goto(labsUrl);
 
@@ -102,7 +139,7 @@ client.on('message', async message => {
 	if (command === 'ehp') {
 		if (args.length < 1) {
 			message.delete();
-			return message.reply('Please specify a username.');
+			return message.reply('please specify a username.');
 		}
 		// Concat
 		const username = args.join(' ');
@@ -111,10 +148,10 @@ client.on('message', async message => {
 		(async () => {
 			try {
 				message.delete();
-				const msg = await message.channel.send('Updating CML... ⏱');
+				const msg = await message.channel.send('Ah yes! It\'s here somewhere... ' + '`' + username + '`');
 				await updateLabs('https://www.crystalmathlabs.com/tracker/update.php?player=' + url);
 				await scrapeLabsAlltime('https://www.crystalmathlabs.com/tracker/track.php?player=' + url + '&time=all');
-				msg.edit('Updated CML, grabbing EHP... 💎');
+				msg.edit('Here it is! Found it!');
 				await scrapeLabsYearly('https://www.crystalmathlabs.com/tracker/track.php?player=' + url + '&time=365d');
 				await scrapeLabsMonthly('https://www.crystalmathlabs.com/tracker/track.php?player=' + url + '&time=31d');
 				await scrapeLabsWeekly('https://www.crystalmathlabs.com/tracker/track.php?player=' + url);
@@ -138,66 +175,106 @@ client.on('message', async message => {
 
 				message.channel.send(ehpEmbed);
 			}
-			catch {
-				message.reply('error occured, make sure you have entered the right username! 📋');
+			catch (err) {
+				message.reply('sorry, I\'m too busy to help you right now!' + '(Make sure you entered the right username!)');
+				console.error(err);
 			}
 		})();
 	}
 	// Clue Command
 	if (command === 'clues') {
 		const username = args.join(' ');
-		const clueStat = await hiscores.getStats(username);
-		console.log(clueStat.main.clues);
-		const clueEmbed = {
-			color: 0x0099ff,
-			title: username,
-			description: 'Data for ' + username + '\'' + 's clues.',
-			thumbnail: {
-				url: 'https://oldschool.runescape.wiki/images/thumb/f/fc/Clue_scroll_%28master%29_detail.png/150px-Clue_scroll_%28master%29_detail.png?596fb',
-			},
-			fields: [
-				{
-					name: '__**All**__',
-					value: 'Score: ' + clueStat.main.clues.all.score + '\n' + 'Rank: ' + clueStat.main.clues.all.rank,
-				},
-				{
-					name: '__**Beginner**__',
-					value: 'Score: ' + clueStat.main.clues.beginner.score + '\n' + 'Rank: ' + clueStat.main.clues.beginner.rank,
-					inline: true,
-				},
-				{
-					name: '__**Easy**__',
-					value: 'Score: ' + clueStat.main.clues.easy.score + '\n' + 'Rank: ' + clueStat.main.clues.easy.rank,
-					inline: true,
-				},
-				{
-					name: '__**Medium**__',
-					value: 'Score: ' + clueStat.main.clues.medium.score + '\n' + 'Rank: ' + clueStat.main.clues.medium.rank,
-					inline: true,
-				},
-				{
-					name: '__**Hard**__',
-					value: 'Score: ' + clueStat.main.clues.hard.score + '\n' + 'Rank: ' + clueStat.main.clues.hard.rank,
-					inline: true,
-				},
-				{
-					name: '__**Elite**__',
-					value: 'Score: ' + clueStat.main.clues.elite.score + '\n' + 'Rank: ' + clueStat.main.clues.elite.rank,
-					inline: true,
-				},
-				{
-					name: '__**Master**__',
-					value: 'Score: ' + clueStat.main.clues.master.score + '\n' + 'Rank: ' + clueStat.main.clues.master.rank,
-					inline: true,
-				},
-			],
-			timestamp: new Date(),
-			footer: {
-				text: 'Wise Old Bot ',
-				icon_url: client.user.avatarURL(),
-			},
-		};
-		message.channel.send({ embed: clueEmbed });
+		console.log('Command Accepted');
+		(async () => {
+			try {
+				const msg = await message.channel.send('I wrote this down, one second... 📜');
+				const clueStat = await hiscores.getStats(username);
+				msg.delete();
+				console.log(clueStat.main.clues);
+				const clueEmbed = {
+					color: 0x0609A2,
+					title: username,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL(),
+						urk: 'https://github.com/tcwork322/EHP-Grabber-Bot' },
+					description: 'Ah! Here you go!',
+					thumbnail: {
+						url: 'https://oldschool.runescape.wiki/images/thumb/f/fc/Clue_scroll_%28master%29_detail.png/150px-Clue_scroll_%28master%29_detail.png?596fb',
+					},
+					fields: [
+						{
+							name: '__**All**__',
+							value: 'Score: ' + clueStat.main.clues.all.score + '\n' + 'Rank: ' + clueStat.main.clues.all.rank,
+						},
+						{
+							name: '__**Beginner**__',
+							value: 'Score: ' + clueStat.main.clues.beginner.score + '\n' + 'Rank: ' + clueStat.main.clues.beginner.rank,
+							inline: true,
+						},
+						{
+							name: '__**Easy**__',
+							value: 'Score: ' + clueStat.main.clues.easy.score + '\n' + 'Rank: ' + clueStat.main.clues.easy.rank,
+							inline: true,
+						},
+						{
+							name: '__**Medium**__',
+							value: 'Score: ' + clueStat.main.clues.medium.score + '\n' + 'Rank: ' + clueStat.main.clues.medium.rank,
+							inline: true,
+						},
+						{
+							name: '__**Hard**__',
+							value: 'Score: ' + clueStat.main.clues.hard.score + '\n' + 'Rank: ' + clueStat.main.clues.hard.rank,
+							inline: true,
+						},
+						{
+							name: '__**Elite**__',
+							value: 'Score: ' + clueStat.main.clues.elite.score + '\n' + 'Rank: ' + clueStat.main.clues.elite.rank,
+							inline: true,
+						},
+						{
+							name: '__**Master**__',
+							value: 'Score: ' + clueStat.main.clues.master.score + '\n' + 'Rank: ' + clueStat.main.clues.master.rank,
+							inline: true,
+						},
+					],
+					timestamp: new Date(),
+					footer: {
+						text: 'Wise Old Bot ',
+						icon_url: client.user.avatarURL(),
+					},
+				};
+				message.channel.send({ embed: clueEmbed });
+			}
+			catch (err) {
+				message.channel.send('Hi-Scores are down or you entered the wrong name, try again.');
+				console.log(err);
+			}
+		})();
+	}
+	// Quote Command
+	if (command === 'quote') {
+		const quotes = [
+			'I\'ve spent my whole life travelling the world, doing quests for people, saving lives, saving villages from terrifying monsters and all that sort of thing.',
+			'Wrong? WRONG? I\'ll tell you what\'s wrong!',
+			'Tut tut tut... Oh well, at least you\'ll never be able to tell the bank about me. They\'ll never listen.',
+			'Oh yes, it was certainly a fitting end to my adventuring days.',
+			'Yes, there was a time, back in the Fourth Age, when we humans wouldn\'t have been able to venture underground. That was before we had magic; the dwarves were quite a threat.',
+			'If Entrana is a land dedicated to the glory of Saradomin, the Wilderness is surely the land of Zamorak.',
+			'If it\'s jungle you want, look no further than the southern regions of Karamja.',
+			'Ah, Uzer. A city much favoured by Saradomin, destroyed by Thammaron, the elder-demon, late in the Third Age.',
+			'There\'s a mighty fire-breathing dragon living underground in the deep Wilderness, known as the King Black Dragon. It\'s a fearsome beast, with a breath that can poison you, freeze you to the ground or incinerate you where you stand.',
+			'Ha ha ha... There are plenty of heroes. Always have been, always will be, until the fall of the world.',
+			'EVEN IF SUCH A CREATURE WERE TO EXIST it would be terribly dangerous for you to get involved in its business.',
+			'Indeed, I have heard of adventurers doing the most shameful and blasphemous deeds believing themselves to be acting on behalf of some false god!',
+			'Saradomin, the great and glorious, gives life to this world.',
+			'Zamorak craves only death and destruction.',
+			'Guthix, calling itself a god of \'balance\', holds no allegiance, but simply aids whatever cause suits its shifting purpose.',
+			'Very little is written about the tribe of the Mahjarrat. They are believed to be from the realm of Freneskae, or Frenaskrae - the spelling in this tongue is only approximate.',
+			'Being noble and virtuous simply doesn\'t put food on the table, and I don\'t intend to sit in banks begging like so many people do these days.',
+			'The jungle\'s filled with nasty creatures. There are vicious spiders that you can hardly see before they try to bite your legs off, and great big jungle ogres.',
+		];
+		message.channel.send(quotes[Math.floor(Math.random() * quotes.length)]);
 	}
 	// Login and out commands for mySQL
 	/* if (command === 'login') {
